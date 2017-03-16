d0fee9b83e733931f23b73769fcfdd4aea901231
SleekPanther <macrobyte2@gmail.com>
2017-03-16 16:59:32 -0400

    fxml works, temporary new window

--- GpaController.java
+++ GpaController.java
@@ -1,38 +1,55 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+
 import javafx.event.*;
 
-public class GpaController {
+public class GpaController  {
 	private GpaView view;
 	private GpaModel model;
 
-	public GpaController(GpaView view, GpaModel model){
-		System.out.println("controller constructor");
-		this.view=view;
-		this.model=model;
+	@FXML private Button button1;
+
+	@FXML
+	public void handleBtn1(ActionEvent event){
+		button1.setText(System.nanoTime()+"");
+	}
+
+	// public GpaController(GpaView view, GpaModel model){
+	// 	System.out.println("controller constructor");
+	// 	this.view=view;
+	// 	this.model=model;
 		
-		addViewEventHandlers();
-	}
-
-	private void addViewEventHandlers(){
-		view.addCalcButtonHandler(new CalcButtonHandler());
-		view.addResetButtonHandler(new ResetButtonHandler());
-	}
+	// 	// addViewEventHandlers();
+	// }
 
 
-	class CalcButtonHandler implements EventHandler<ActionEvent> {
-		@Override
-		public void handle(ActionEvent e) {
-			model.calcGpaOverall();
-			view.setGpaOverall(model.getGpaOverall());
-		}
+	// private void addViewEventHandlers(){
+	// 	view.addCalcButtonHandler(new CalcButtonHandler());
+	// 	view.addResetButtonHandler(new ResetButtonHandler());
+	// }
 
-		//do more complex calutations in method? to extract try/catch
-	}
 
-	class ResetButtonHandler implements EventHandler<ActionEvent> {
-		@Override
-		public void handle(ActionEvent e) {
-			view.setGpaOverall("0.0");
-		}
-	}
+	// class CalcButtonHandler implements EventHandler<ActionEvent> {
+	// 	@Override
+	// 	public void handle(ActionEvent e) {
+	// 		model.calcGpaOverall();
+	// 		view.setGpaOverall(model.getGpaOverall());
+	// 	}
+
+	// 	//do more complex calutations in method? to extract try/catch
+	// }
+
+	// class ResetButtonHandler implements EventHandler<ActionEvent> {
+	// 	@Override
+	// 	public void handle(ActionEvent e) {
+	// 		view.setGpaOverall("0.0");
+	// 	}
+	// }
 
 }
--- GpaView.java
+++ GpaView.java
@@ -1,14 +1,17 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.control.*;
-// import javafx.scene.control.Button;
-// import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 
-import java.awt.event.ActionListener;
 import java.util.*;
 
 public class GpaView extends Application {
@@ -31,57 +34,73 @@ public class GpaView extends Application {
 	}
 
 	@Override
-	public void start(Stage stage) throws Exception {
-		System.out.println("start");
-		primaryStage=stage;
-		setUpGui();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+	// @Override
+	// public void start(Stage stage) throws Exception {
+	// 	System.out.println("start");
+
+	// 	Parent rootParent = FXMLLoader.load(getClass().getResource("ui.fxml"));
+ //        // primaryStage.setTitle("Hello World");
+ //        // primaryStage.setScene(new Scene(root, 300, 275));
+ //        // primaryStage.show();
+
+
+	// 	primaryStage=stage;
+	// 	// setUpGui();
+
+	// 	GpaModel model = new GpaModel();
+	// 	GpaController controller = new GpaController(this, model);
 		
-		GpaModel model = new GpaModel();
-		GpaController controller = new GpaController(this, model);
+	// 	primaryStage.setScene(new Scene(rootParent, 300, 275));		//what are these dimensions??????????
+	// 	primaryStage.show();
+	// }
+
+	// public static void main(String[] args) {
+	// 	System.out.println("main");
+	// 	launch(args);
+	// }
+
+	// private void setUpGui(){
+	// 	calcPane=new HBox();
+	// 	calcGpaOverallButton = new Button("Calculate Gpa");
+	// 	gpaOverallLabel = new Label(gpaOverallDefaultText);
+	// 	resetButton=new Button("Reset All");
+	// 	calcPane.getChildren().addAll(calcGpaOverallButton, gpaOverallLabel, resetButton);
+
+	// 	mainPane = new VBox();
+	// 	mainPane.getChildren().addAll(calcPane);
+
+	// 	mainScene = new Scene(mainPane);
 		
-		primaryStage.show();
-	}
-
-	public static void main(String[] args) {
-		System.out.println("main");
-		launch(args);
-	}
-
-	private void setUpGui(){
-		calcPane=new HBox();
-		calcGpaOverallButton = new Button("Calculate Gpa");
-		gpaOverallLabel = new Label(gpaOverallDefaultText);
-		resetButton=new Button("Reset All");
-		calcPane.getChildren().addAll(calcGpaOverallButton, gpaOverallLabel, resetButton);
-
-		mainPane = new VBox();
-		mainPane.getChildren().addAll(calcPane);
-
-		mainScene = new Scene(mainPane);
-		
-		primaryStage.setTitle("GPA Calculator");
-		primaryStage.setScene(mainScene);
-		primaryStage.setHeight(windowHeight);
-		primaryStage.setWidth(windowWidth);
-		primaryStage.setResizable(false);
-	}
+	// 	primaryStage.setTitle("GPA Calculator");
+	// 	primaryStage.setScene(mainScene);
+	// 	primaryStage.setHeight(windowHeight);
+	// 	primaryStage.setWidth(windowWidth);
+	// 	primaryStage.setResizable(false);
+	// }
 	
-	public void addCalcButtonHandler(EventHandler<ActionEvent> calcButtonHandler){
-		calcGpaOverallButton.setOnAction(calcButtonHandler);
-	}
+	// public void addCalcButtonHandler(EventHandler<ActionEvent> calcButtonHandler){
+	// 	calcGpaOverallButton.setOnAction(calcButtonHandler);
+	// }
 
-	public void addResetButtonHandler(EventHandler<ActionEvent> resetButtonHandler){
-		resetButton.setOnAction(resetButtonHandler);
-	}
+	// public void addResetButtonHandler(EventHandler<ActionEvent> resetButtonHandler){
+	// 	resetButton.setOnAction(resetButtonHandler);
+	// }
 
 
-	public void setGpaOverall(String gpa){
-		gpaOverallLabel.setText(gpa);
-	}
+	// public void setGpaOverall(String gpa){
+	// 	gpaOverallLabel.setText(gpa);
+	// }
 
-	//set text passing a number
-	public void setGpaOverall(double gpa){
-		setGpaOverall(gpa+"");		//convert to string
-	}
+	// //set text passing a number
+	// public void setGpaOverall(double gpa){
+	// 	setGpaOverall(gpa+"");		//convert to string
+	// }
 
 }
