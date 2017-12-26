537b02b7590d82011806364d34dcb05291713f93
SleekPanther <macrobyte2@gmail.com>
2017-12-26 08:55:24 -0500

    Handle multiple labels, validate current gpa

--- GpaController.java
+++ GpaController.java
@@ -12,13 +12,17 @@ import java.util.*;
 public class GpaController implements Initializable {
 	private GpaModel model = new GpaModel();
 
-	//don't need panes??
 	// @FXML private VBox mainPane;
 	// 	@FXML private GridPane classesPane;
-		@FXML private TextField class1Title;
-		@FXML private Label class1TitleError;
 		@FXML private TextField class1Grade;
 		@FXML private Label class1GradeError;
+
+		@FXML private TextField class1Credits;
+		@FXML private Label class1CreditsError;
+
+		@FXML private TextField currentGPA;
+		@FXML private Label currentGPAError;
+
 	// 	@FXML private VBox calcPane;
 			@FXML private Button calcGpaOverallButton;
 			@FXML private Label gpaOverallLabel;
@@ -32,13 +36,16 @@ public class GpaController implements Initializable {
 		});
 
 		class1Grade.focusedProperty().addListener(new TextFieldListener(class1Grade, class1GradeError));
+		class1Credits.focusedProperty().addListener(new TextFieldListener(class1Credits, class1CreditsError));
+		currentGPA.focusedProperty().addListener(new TextFieldListener(currentGPA, currentGPAError, "currentGPA"));
 
-		//Request focuse must be AFTER he FXML is finished loading
+		//Also handle acents when pressing Enter, not many buttons
+
+		//Request focus must be AFTER he FXML is finished loading
 		Platform.runLater(new Runnable() {
 			@Override
 			public void run() {
 				class1Grade.requestFocus();
-				
 			}
 		});
 	}
@@ -56,17 +63,24 @@ public class GpaController implements Initializable {
 
 	@FXML
 	public void handleClass1Grade(ActionEvent event){
-		gpaOverallLabel.setText( ((TextInputControl) event.getSource()).getText() );
+		// gpaOverallLabel.setText( ((TextInputControl) event.getSource()).getText() );
+		System.out.println("Pressed Enter");
+	}
+
+	@FXML
+	public void handleClass1Credits(ActionEvent event){
+		// gpaOverallLabel.setText( ((TextInputControl) event.getSource()).getText() );
 	}
 
 	@FXML
 	public void handleCalcGpaOverallButton(ActionEvent event){
-		model.calcGpaOverall();	//pass in classes to calc
+		model.calcGpaOverall();		//need pass in classes to calculate
 		gpaOverallLabel.setText(numToText( model.getGpaOverall() ));
 	}
 
 	@FXML
 	public void handleResetButton(ActionEvent event){
+		model.reset();
 		gpaOverallLabel.setText("0.0");
 	}
 
@@ -83,24 +97,35 @@ public class GpaController implements Initializable {
 	class TextFieldListener implements ChangeListener<Boolean> {
 		private final TextField numericTextField;
 		private final Label errorLabel;
+		private String type;
 
 		TextFieldListener(TextField numericTextField, Label errorLabel) {
+			this(numericTextField, errorLabel, "");
+		}
+		TextFieldListener(TextField numericTextField, Label errorLabel, String type) {
 			this.numericTextField = numericTextField;
 			this.errorLabel = errorLabel;
+			this.type=type;
 		}
 
 		@Override
 		public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-			if(!newValue){	// check if focus gained or lost
-				//check if INVALID
-				String errorText = "err text";
-				errorLabel.setText(errorText);
-				// this.numericTextField.setText(getFormattedText(this.numericTextField.getText());
+			if(!newValue){		// check if focus gained or lost
+				String errorText = "";
+				if(type.equals("currentGPA")){
+					errorText=model.getGPAErrorTextIfInvalid(numericTextField.getText());
+					errorLabel.setText(errorText);		//Always set error text (clears error if they fixed the mistake)
+					if(errorText.equals("")){
+						gpaOverallLabel.setText("model.calcGpaOverall()");
+					}
+				}else{
+					//check if INVALID
+					errorText=" err";
+					// String errorText = model.getNumericErrorTextIfInvalid();
+					errorLabel.setText(numericTextField.getText()+errorText);
+				}
 			}
 		}
-		// public String getFormatedText(String str) {
-		// 	return str;
-		// }
 	}
 
 }
--- GpaModel.java
+++ GpaModel.java
@@ -1,18 +1,46 @@
 
-
 public class GpaModel {
-	private double gpaUpperBound = 4;
-	private double gpaLowerBound = 0;
-	//more limits on valid inputs: grades, 0-100 etc
+	private final double GPA_UPPER_BOUND = 4;
+	private final double GPA_LOWER_BOUND = 0;
+	private final double CREDITS_LOWER_BOUND = 0;
+	
 
 	private double gpaOverall;
 	//list of classes?
 
 	public GpaModel() {
-		
+
+	public String getGPAErrorTextIfInvalid(String number){
+		String errorText="";
+
+		double gpa;
+		if(!isNumeric(number)){
+			errorText = "Error: GPA must be a number";
+		}
+		else{
+			gpa = Double.parseDouble(number);
+			if(gpa > GPA_UPPER_BOUND){
+				errorText="Error: GPA can't be > 4.0";
+			}
+			if(gpa < GPA_LOWER_BOUND){
+				errorText="Error: GPA can't be negative";
+			}
+		}
+		return errorText;
 	}
 
-	public void calcGpaOverall(){	//throws exception?
+	public static boolean isNumeric(String number){
+		try{
+			Double.parseDouble(number);
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
+
+
+	public void calcGpaOverall(){	//helper methods throw exceptions
 		double total=0;
 		total=11;
 		//for(){} all classes passed in
@@ -23,6 +51,10 @@ public class GpaModel {
 
 	public double getGpaOverall(){
 		return gpaOverall;
+	}
+
+	public void reset(){
+
 	}
 
 }
